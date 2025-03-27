38ccab6d89bec6549beb3ae159029a4c49669883
MisacAguilar08 <misacaguilar@gmail.com>
2025-03-26 20:29:42 -0600

    course

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,14 @@ public class Main {
         func.diaSemana(2);
         func.isVocal('o');
         func.sumaPares(10);
-        func.impNumInvertidos(100,200);
+        func.impNumInvertidos(100,40);
         func.tablaMultiplicar();
         func.triangulo(2);
-        func.triangulo(2);
+        func.trianguloSpace(2);
+        func.rectangulo(5,9);
+        func.doWhilenumeroMayor();
+        func.doWhileImparPar();
+        func.whileSuma(5);
         System.out.println(func.whileFactorial(6));
         func.whileFactoerialScenner();
 
